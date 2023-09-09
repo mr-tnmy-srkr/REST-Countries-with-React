db98da51e3de699570a1dc6eb1bcfdd40a7a08b9
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-09-10 03:03:48 +0530

    flag added

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -9,7 +9,7 @@ const Countries = () => {
   //   need to focus
   const [visitedCounties, setVisitedCounties] = useState([]);
   //recap
-
+  const [visitedFlags, setVisitedFlags] = useState([]);
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
@@ -22,18 +22,33 @@ const Countries = () => {
 
   //   need to focus
   const handleVisitedCountry = (country) => {
-    console.log("visited");
+    // console.log("visited");
     // console.log(country);
     // visitedCounties.push(country)   eta diye hobe na
     const newVisitedCountries = [...visitedCounties, country];
     setVisitedCounties(newVisitedCountries);
   };
 
+  //recap
+  const handleVisitedFlag = (flag) => {
+    // console.log('flag');
+    const newVisitedFlags = [...visitedFlags, flag];
+    setVisitedFlags(newVisitedFlags);
+  };
+
+/* concept
+remove item from an array in a state.
+use filter to select all the elements except the one you want to remove 
+more on react.dev->updating array in state
+*/
+
+
   return (
     <div>
       <h3>Countries : {countries.length}</h3>
 
       {/* need to focus */}
+      {/* visited countries */}
       <div>
         <h4>Visited Countries : {visitedCounties.length}</h4>
         <ul>
@@ -42,12 +57,22 @@ const Countries = () => {
           ))}
         </ul>
       </div>
+      {/* recap */}
+      {/* visited flags */}
+      <div className="flag-container">
+        {visitedFlags.map((flag,idx) => (
+          <img key={idx} src={flag}></img>
+        ))}
+      </div>
+      {/* display countries */}
       <div className="country-container">
         {countries.map((country) => (
           <Country
             key={country.cca3}
             //   need to focus
             handleVisitedCountry={handleVisitedCountry}
+            // recap
+            handleVisitedFlag={handleVisitedFlag}
             country={country}
           ></Country>
         ))}
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,34 +1,42 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react';
-import './Country.css'
+import { useState } from "react";
+import "./Country.css";
 
 //   need to focus
-const Country = ({country, handleVisitedCountry}) => {
-    const{name,flags,population,area,cca3} = country;
-    // console.log(country);
-    // console.log(handleVisitedCountry);
+const Country = ({ country, handleVisitedCountry, handleVisitedFlag }) => {
+  const { name, flags, population, area, cca3 } = country;
+  // console.log(country);
+  // console.log(handleVisitedCountry);
 
-const [visited ,setVisited] = useState(false);
+  const [visited, setVisited] = useState(false);
 
-const handleVisited =() =>{
-    setVisited(!visited)
-}
-    return (
-        <div className={`country ${visited ? 'visited' : 'non-visited'}`}>
-            <h3 style={{color : visited? 'blue' : 'green'}}>Name : {name?.common}</h3>
-            <img src={flags.png} alt="" />
-            <p>Population : {population}</p>
-            <p>Area : {area}</p>
-            <p><small>Code : {cca3}</small></p>
+  const handleVisited = () => {
+    setVisited(!visited);
+  };
+  return (
+    <div className={`country ${visited ? "visited" : "non-visited"}`}>
+      <h3 style={{ color: visited ? "blue" : "green" }}>
+        Name : {name?.common}
+      </h3>
+      <img src={flags.png} alt="" />
+      <p>Population : {population}</p>
+      <p>Area : {area}</p>
+      <p>
+        <small>Code : {cca3}</small>
+      </p>
 
-           {/* need to focus */}
-            <button onClick={() =>handleVisitedCountry(country)}>mark Visited</button>
-            <br />
-            <br />
-            <button onClick={handleVisited}>{visited? 'Visited' : 'Going'}</button>
-            {visited ? 'I have visited this country.' : 'I want to visit'}
-        </div>
-    );
+      {/* need to focus */}
+      <button onClick={() => handleVisitedCountry(country)}>
+        mark Visited
+      </button>
+      <br />
+      {/* recap */}
+      <button onClick={(()=>handleVisitedFlag(country.flags.png))}>Add Flag</button>
+      <br />
+      <button onClick={handleVisited}>{visited ? "Visited" : "Going"}</button>
+      {visited ? "I have visited this country." : "I want to visit"}
+    </div>
+  );
 };
 
 export default Country;
