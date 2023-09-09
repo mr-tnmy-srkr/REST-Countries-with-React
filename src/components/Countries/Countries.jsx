017b2bd15cd0fb4a5dae43a33f1fd2596096daae
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-09-10 02:26:57 +0530

    visitedCountry count+ namelist added

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,27 +1,59 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/country";
+import "./Countries.css";
 
 const Countries = () => {
+  const [countries, setCountries] = useState([]);
 
-const [countries, setCountries] = useState([]);
+  //   need to focus
+  const [visitedCounties, setVisitedCounties] = useState([]);
+  //recap
 
-useEffect(()=>{
-fetch('https://restcountries.com/v3.1/all')
-.then(res =>res.json())
-.then(data=>setCountries(data))
-.catch(err => {console.log('Error', err);})
 
-},[]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data))
+      .catch((err) => {
+        console.log("Error", err);
+      });
+  }, []);
 
-    return (
-        <div>
-            <h3>Countries : {countries.length}</h3>
-            {
-                countries.map(country => <Country key={country.cca3} country={country}></Country>)
-            }
-        </div>
-    );
+  //   need to focus
+  const handleVisitedCountry = (country) => {
+    console.log("visited");
+    // console.log(country);
+    // visitedCounties.push(country)   eta diye hobe na
+    const newVisitedCountries = [...visitedCounties, country];
+    setVisitedCounties(newVisitedCountries);
+  };
+
+  return (
+    <div>
+      <h3>Countries : {countries.length}</h3>
+
+      {/* need to focus */}
+      <div>
+        <h4>Visited Countries : {visitedCounties.length}</h4>
+        <ul>
+          {visitedCounties.map((country) => (
+            <li key={country.cca3}> {country.name.common}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="country-container">
+        {countries.map((country) => (
+          <Country
+            key={country.cca3}
+            //   need to focus
+            handleVisitedCountry={handleVisitedCountry}
+            country={country}
+          ></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,32 @@
+/* eslint-disable react/prop-types */
+import { useState } from 'react';
 import './Country.css'
-const Country = ({country}) => {
-    const{name,flags} = country;
-    console.log(country);
-    // console.log(name);
+
+//   need to focus
+const Country = ({country, handleVisitedCountry}) => {
+    const{name,flags,population,area,cca3} = country;
+    // console.log(country);
+    // console.log(handleVisitedCountry);
+
+const [visited ,setVisited] = useState(false);
+
+const handleVisited =() =>{
+    setVisited(!visited)
+}
     return (
-        <div className='country'>
-            <h3>Name : {name?.common}</h3>
+        <div className={`country ${visited ? 'visited' : 'non-visited'}`}>
+            <h3 style={{color : visited? 'blue' : 'green'}}>Name : {name?.common}</h3>
             <img src={flags.png} alt="" />
+            <p>Population : {population}</p>
+            <p>Area : {area}</p>
+            <p><small>Code : {cca3}</small></p>
+
+           {/* need to focus */}
+            <button onClick={() =>handleVisitedCountry(country)}>mark Visited</button>
+            <br />
+            <br />
+            <button onClick={handleVisited}>{visited? 'Visited' : 'Going'}</button>
+            {visited ? 'I have visited this country.' : 'I want to visit'}
         </div>
     );
 };
